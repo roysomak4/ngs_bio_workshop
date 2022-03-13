072a0a6a173ebbc56951dc247d91a5c86f57cfe2
Somak Roy <roysomak4@gmail.com>
2022-03-13 05:20:40 -0400

    Updated vm config script to include persistent NFS mounts, correct version of mambaforge, and shell init after mambaforge is installed

--- instructors/scripts/configure_vm.sh
+++ instructors/scripts/configure_vm.sh
@@ -2,19 +2,20 @@
 # update repos and install dependencies
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y unzip nfs-common
+sudo apt install -y unzip nfs-common tree
 
 # mount nfs assets share on vm
 mkdir assets
-sudo mount -t nfs -o rw,soft,intr,noacl,noatime,timeo=900,retrans=3,proto=tcp,vers=4 159.203.87.19:/exports/assets assets
+sudo bash -c 'echo -e "159.203.87.19:/exports/assets\t/home/bioseq/assets\tnfs\trw,soft,intr,noacl,noatime,timeo=900,retrans=3,proto=tcp,vers=4\t0\t0" >> /etc/fstab'
+sudo mount -a -v
 sudo chown -R bioseq assets
 
 # install ngs apps
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-pypy3-Linux-x86_64.sh
-chmod +x Mambaforge-pypy3-Linux-x86_64.sh 
-./Mambaforge-pypy3-Linux-x86_64.sh -b 
-rm -r Mambaforge-pypy3-Linux-x86_64.sh
-mamba install -y -c bioconda bwa samtools fastp fastqc sambamba varscan==2.4.4 abra2
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+chmod +x Mambaforge-Linux-x86_64.sh 
+./Mambaforge-Linux-x86_64.sh -b 
+rm -r Mambaforge-Linux-x86_64.sh
+mambaforge/bin/mamba install -y -c bioconda bwa samtools fastp fastqc sambamba varscan==2.4.4 abra2
 
 # Download annovar (refer to download link in private repo - only for administrator)
 wget http://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz
@@ -32,7 +33,13 @@ mkdir data
 gdrive_download.sh 1p7vZebLxPkvlUrHAsyrh44WyS5Q4iV2I sample_data.zip
 mv Downloads/sample_data.zip data/
 unzip data/sample_data.zip -d data/
+mv data/grip_course_hd701_R1.fq.gz data/hd701_R1.fq.gz
+mv data/grip_course_hd701_R2.fq.gz data/hd701_R2.fq.gz
 
 # clean up
 rm data/sample_data.zip data/grip_course.bed
 rm -r Downloads
+
+# update environment
+mambaforge/bin/mamba init -q
+sudo reboot
