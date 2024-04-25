d6f050b2926bffe98df09430bdcfa61322d0cf8d
Somak Roy <roysomak4@gmail.com>
2024-04-25 12:52:28 -0400

    fix: updated SSHD setting to allow password login from terminal

--- instructors/scripts/create_user.sh
+++ instructors/scripts/create_user.sh
@@ -9,6 +9,7 @@ useradd -m -U bioseq -s /bin/bash
 echo -e "bioseq123\nbioseq123" | passwd bioseq
 usermod -aG sudo bioseq
 sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+sed -i "s/^KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/" /etc/ssh/sshd_config
 systemctl restart sshd
 cp -R /root/.ssh /home/bioseq/
 chown -R bioseq:bioseq /home/bioseq/.ssh
